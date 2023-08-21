23ae580e90937d6208c4edc7859d3af8c167d7dd
Ian Bygrave <ian@bygrave.me.uk>
2023-08-21 14:51:28 +0100

    Use uefi::Result instead of expect

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use uefi::proto::device_path::text::DevicePathFromText;
 use uefi::proto::ProtocolPointer;
 use uefi::table::boot::SearchType;
 use uefi::Identify;
-use uefi::{prelude::*, CStr16};
+use uefi::{prelude::*, CStr16, Result};
 
 const SERIAL_DEVICE_PATH: &CStr16 =
     cstr16!("PciRoot(0x0)/Pci(0x1,0x0)/Serial(0x1)/Uart(115200,8,N,1)");
@@ -15,32 +15,24 @@ const SERIAL_DEVICE_PATH: &CStr16 =
 fn handle_from_path<P: ProtocolPointer + ?Sized>(
     boot_services: &BootServices,
     path: &CStr16,
-) -> Handle {
+) -> Result<Handle> {
     let device_path_from_text_handle = *boot_services
-        .locate_handle_buffer(SearchType::ByProtocol(&DevicePathFromText::GUID))
-        .expect("DevicePathFromText is missing")
+        .locate_handle_buffer(SearchType::ByProtocol(&DevicePathFromText::GUID))?
         .first()
-        .expect("DevicePathFromText is missing");
+        .ok_or(uefi::Status::NOT_FOUND)?;
 
     let device_path_from_text = boot_services
-        .open_protocol_exclusive::<DevicePathFromText>(device_path_from_text_handle)
-        .expect("DevicePathFromText unavailable");
+        .open_protocol_exclusive::<DevicePathFromText>(device_path_from_text_handle)?;
 
-    boot_services
-        .locate_device_path::<P>(
-            &mut device_path_from_text
-                .convert_text_to_device_path(path)
-                .expect("Device path not valid"),
-        )
-        .expect("Device path not found")
+    let mut device_path = device_path_from_text.convert_text_to_device_path(path)?;
+
+    boot_services.locate_device_path::<P>(&mut device_path)
 }
 
-fn test_serial(boot_services: &BootServices) {
-    let serial_handle = handle_from_path::<Serial>(boot_services, SERIAL_DEVICE_PATH);
+fn test_serial(boot_services: &BootServices) -> Result<()> {
+    let serial_handle = handle_from_path::<Serial>(boot_services, SERIAL_DEVICE_PATH)?;
 
-    let mut serial_protocol = boot_services
-        .open_protocol_exclusive::<Serial>(serial_handle)
-        .expect("Serial device unavailable");
+    let mut serial_protocol = boot_services.open_protocol_exclusive::<Serial>(serial_handle)?;
 
     match serial_protocol.write("Hello world!".as_bytes()) {
         Err(count) => {
@@ -50,6 +42,7 @@ fn test_serial(boot_services: &BootServices) {
             info!("serial.write() Ok");
         }
     };
+    Ok(())
 }
 
 #[entry]
@@ -58,7 +51,7 @@ fn main(_image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     let boot_services = system_table.boot_services();
 
     system_table.boot_services().stall(10_000_000);
-    test_serial(boot_services);
+    test_serial(boot_services).unwrap();
     system_table.boot_services().stall(10_000_000);
 
     Status::SUCCESS
